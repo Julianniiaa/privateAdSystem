4adbffed494a871a6bf4d919b4ddbd7870264932
Julianniiaa <ykulichyk@yandex.by>
2022-06-14 13:35:02 +0300

    Initialization

---
+++ src/main/java/com/example/privateadsystem/PrivateAdSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.privateadsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateAdSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateAdSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/privateadsystem/PrivateAdSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.privateadsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateAdSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
